88adf420187ef0e8135ed1786d72a26777cf642f
Rodrigo Vasquez <ro.vasquezn@duocuc.cl>
2022-09-21 19:40:37 -0300

    primer commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router: Router){}
+
+
+  irAlInicio(){
+    this.router.navigate(['/inicio'])
+}
+
+
+  ngOnInit() {
+  }
+
+}
