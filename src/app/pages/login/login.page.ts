7af2b1a32ea09eb7c9443ced588a4be1ae2dfa99
Rodrigo Vasquez <ro.vasquezn@duocuc.cl>
2022-09-22 16:23:27 -0300

    Login arreglado

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { 
+  FormGroup, FormControl, Validators, FormBuilder
+} from '@angular/forms';
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,13 +11,26 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router){}
+  formularioLogin: FormGroup;
 
+  constructor(private router: Router, public fb: FormBuilder){
 
+    this.formularioLogin = this.fb.group({
+      'usuario': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+
+  };
+
+ //constructor(private router: Router){}
   irAlInicio(){
     this.router.navigate(['/inicio'])
 }
 
+ irArecuperar(){
+    this.router.navigate(['/recuperar'])
+
+}
 
   ngOnInit() {
   }
